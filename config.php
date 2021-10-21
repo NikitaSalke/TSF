bef99d6673aee348c6359cccde3beb9414363a59
NikitaSalke <90171745+NikitaSalke@users.noreply.github.com>
2021-10-21 10:32:58 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="sparks_bank";
+
+	$conn = mysqli_connect("localhost" , "root" , ""  , "sparks_bank");
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
